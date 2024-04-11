596ddec17a8171e9ada337c62ec8d1142740fc15
Jake Donham <jaked@github.com>
2024-04-11 16:11:47 -0700

    don't inline vite react plugin preamble
    I don't know why I thought this worked; but it seems like the renderer somehow runs in a different window context the React render, so the preamble needs to be injected as a script tag.

--- packages/vscode/src/vitaleRenderer.tsx
+++ packages/vscode/src/vitaleRenderer.tsx
@@ -3,12 +3,6 @@ import type { ActivationFunction } from "vscode-notebook-renderer";
 
 const cellIdRegex = /^([^?]+\.vnb)\?cellId=([a-zA-z0-9_-]{21})\.([a-z]+)$/;
 
-namespace window {
-  export let $RefreshReg$: () => void;
-  export let $RefreshSig$: () => (type: any) => any;
-  export let __vite_plugin_react_preamble_installed__: boolean;
-}
-
 export const activate: ActivationFunction = (_context) => ({
   async renderOutputItem(outputItem, element) {
     element.replaceChildren();
@@ -26,12 +20,19 @@ export const activate: ActivationFunction = (_context) => ({
     root.id = `cell-output-root-${cellId}`;
     element.appendChild(root);
 
-    const RefreshRuntime = await import(`http://localhost:5173/@react-refresh`);
-    RefreshRuntime.default.injectIntoGlobalHook(window);
-    window.$RefreshReg$ = () => {};
-    window.$RefreshSig$ = () => (type) => type;
-    window.__vite_plugin_react_preamble_installed__ = true;
+    const script = document.createElement("script");
+    script.type = "module";
+    script.innerText = `
+import RefreshRuntime from "http://localhost:5173/@react-refresh";
+RefreshRuntime.injectIntoGlobalHook(window);
+window.$RefreshReg$ = () => {};
+window.$RefreshSig$ = () => (type) => type;
+window.__vite_plugin_react_preamble_installed__ = true;
 
-    await import(`http://localhost:5173/${id}&t=${Date.now()}`);
+document.getElementsByTagName("base")[0].href = "http://localhost:5173/";
+
+import("http://localhost:5173/${id}&t=${Date.now()}");
+`;
+    element.appendChild(script);
   },
 });
